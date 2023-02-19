edb291ac9d88a4ec10e60979741882da89cfd5b9
William Fernandes <wffernandes@twadev.com.br>
2023-02-19 10:40:54 -0300

    Comit final com todos os arquivos do projeto.

---
+++ sales-api/src/config/auth/checkToken.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import { promisify } from "util";
+
+import AuthException from "./AuthException.js";
+
+import { API_SECRET } from "../constants/secrets.js";
+import { UNAUTHORIZED, INTERNAL_SERVER_ERROR } from "../constants/httpStatus.js";
+
+const emptySpace = " ";
+
+export default async (req, res, next) => {
+    try {
+        const { authorization } = req.headers;
+        if (!authorization) {
+            throw new AuthException(UNAUTHORIZED, "Access token was not informed.");
+        }
+        let accessToken = authorization;
+        if (accessToken.toLowerCase().includes(emptySpace)) {
+            accessToken = accessToken.split(emptySpace)[1];
+        }
+        const decoded = await promisify(jwt.verify)(accessToken, API_SECRET);
+        req.authUser = decoded.authUser;
+        return next();
+    } catch (err) {
+        const status = err.status ? err.status : INTERNAL_SERVER_ERROR
+        return res.status(status).json({
+            status, 
+            message: err.message,
+        });        
+    }
+};
